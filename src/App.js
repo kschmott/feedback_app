c7b1c8a70f1daed7540ebb03efd9f454ff45c749
kschmott <kschmott@umich.edu>
2022-04-21 18:49:33 -0400

    added average rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import { useState } from "react";
 import FeedbackData from "./Data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = (id) => {
@@ -13,6 +15,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
