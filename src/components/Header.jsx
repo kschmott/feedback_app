a9bd96a081c7b44dce5ccd9b28a1071c11b4a3ea
kschmott <kschmott@umich.edu>
2022-04-20 23:04:55 -0400

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types"
+
+
+function Header({ text }) {
+  return (
+   <header>
+       <div className="container">
+           <h1>{text}</h1>
+       </div>
+   </header>
+  )
+}
+Header.defaultProps = {
+    text : 'Feedback UI',
+}
+export default Header
