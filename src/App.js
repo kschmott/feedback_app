5e0e97b732102ccb43f92cdae531eb273d50fc2d
kschmott <kschmott@umich.edu>
2022-04-21 16:18:03 -0400

    added delete comment functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Header from "./components/Header";
-
+import FeedbackList from "./components/FeedbackList";
+import { useState } from "react";
+import FeedbackData from "./Data/FeedbackData";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <h1>My App</h1>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
-
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    color: textColor,
+    backgroundColor: bgColor,
+  };
   return (
-   <header>
-       <div className="container">
-           <h1>{text}</h1>
-       </div>
-   </header>
-  )
+    <header style={headerStyles}>
+      <div className="container">
+        <h1>{text}</h1>
+      </div>
+    </header>
+  );
 }
 Header.defaultProps = {
-    text : 'Cool UI',
-}
-export default Header
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
+Header.propTypes = {
+  text: PropTypes.string,
+  bgcolor: PropTypes.string,
+  textColor: PropTypes.string,
+};
+export default Header;
