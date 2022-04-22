6bf51d14ec7cb9361f7110f8a26b5d8068e0d10b
kschmott <kschmott@umich.edu>
2022-04-22 17:32:14 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import FeedbackData from "./Data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import { v4 as uuidv4 } from "uuid";
+import AboutPage from "./pages/AboutPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -19,14 +22,29 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+                <AboutIconLink />
+              </>
+            }
+          />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this Project</h1>
+        <p>
+          This my first react app and I don't understand much of what I've
+          written down
+        </p>
+        <p>Version 1.0.0</p>
+
+        <p>
+          <Link to="/">Back to Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
